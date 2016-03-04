87fb717829c26fc316f29bd206bcd7d259502ed5
MaxMommersteeg <maxmommersteeg@hotmail.com>
2016-03-04 22:12:31 +0100

    Multiple changes
    Improved icon.
    Model bugfix.

--- Android-final/app/src/main/java/com/maxmommersteeg/max/android_final/Model/Person.java
+++ Android-final/app/src/main/java/com/maxmommersteeg/max/android_final/Model/Person.java
@@ -80,7 +80,7 @@ public class Person {
     }
 
     public String getFullName() {
-        if(middleName == null || middleName.isEmpty())
+        if(middleName.equals(null) || middleName.isEmpty())
             return firstName + " " + lastName;
         return firstName + " " + middleName + " " + lastName;
     }
