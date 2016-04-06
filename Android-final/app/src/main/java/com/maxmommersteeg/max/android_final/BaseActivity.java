7de0d0b8415eb0ee7b53cacf8e724dcec1b48278
MaxMommersteeg <maxmommersteeg@hotmail.com>
2016-04-06 12:02:41 +0200

    Improved BaseActivity

--- Android-final/app/src/main/java/com/maxmommersteeg/max/android_final/BaseActivity.java
+++ Android-final/app/src/main/java/com/maxmommersteeg/max/android_final/BaseActivity.java
@@ -2,6 +2,9 @@ package com.maxmommersteeg.max.android_final;
 
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.lang.Override;
 
 /**
  * Created by Max on 4-4-2016.
@@ -12,6 +15,47 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected static final String  BASE_API_URL = "https://api.myjson.com/bins/2lzia";
     protected static final String ARG_PERSON_ID = "ARG_PERSON_ID";
 
-    public BaseActivity() {
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show():
+    }
+
+    protected void showAlert(String msg) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getResources().getString(R.string.app_name))
+                .setMessage(msg)
+                .setCancelable(false)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create().show();
+    }
+
+    protected void showProgress(String msg) {
+        if (mProgressDialog != null && mProgressDialog.isShowing())
+            dismissProgress();
+        mProgressDialog = ProgressDialog.show(this, getResources().getString(R.string.app_name), msg);
+    }
+
+    protected void dismissProgress() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
 }
