bbfb32affd1ebcbcbe61d226d93e8ed8d4033ba8
MaxMommersteeg <maxmommersteeg@hotmail.com>
2016-04-11 14:24:14 +0200

    Added Preference variables to BaseActivity

--- Android-final/app/src/main/java/com/maxmommersteeg/max/android_final/BaseActivity.java
+++ Android-final/app/src/main/java/com/maxmommersteeg/max/android_final/BaseActivity.java
@@ -18,6 +18,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected static final String ARG_PERSON_OBJECT = "ARG_PERSON_OBJECT";
     protected static final String ALIAS_PREFERENCE_KEY = "ALIAS_PREFERENCE_KEY";
 
+    protected SharedPreferences mPreferences;
+    protected SharedPreferences.Editor mPreferenceEditor;
 
     protected void showAlert(String msg) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
